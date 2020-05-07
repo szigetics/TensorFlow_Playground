cc4c0aaba3835bb81fda226d89b7aa8ddc57ee53
Csaba Szigeti <csaba.szigeti@pix4d.com>
2020-05-07 19:14:36 +0200

    lower epoch

--- 3_iOS_Face_Mask_Detector/Keras_Model/train_mask_detector.py
+++ 3_iOS_Face_Mask_Detector/Keras_Model/train_mask_detector.py
@@ -38,7 +38,7 @@ args = vars(ap.parse_args())
 # initialize the initial learning rate, number of epochs to train for,
 # and batch size
 INIT_LR = 1e-4
-EPOCHS = 20
+EPOCHS = 3
 BS = 32
 
 # grab the list of images in our dataset directory, then initialize
